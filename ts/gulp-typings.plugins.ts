68a65b55db5ec45b534badaabaad96a8c2cfd5c5
Phil Kunz <github@philkunz.com>
2016-07-26 00:38:00 +0200

    Update gulp-typings.plugins.ts

--- ts/gulp-typings.plugins.ts
+++ ts/gulp-typings.plugins.ts
@@ -1,5 +1,4 @@
 /// <reference path="./typings/index.d.ts" />
-export let beautylog = require("beautylog");
 export let path = require("path");
 export let through2 = require("through2");
 export let typingsCore = require("typings-core");
